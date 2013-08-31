3e2652da87cdc6cdc36fb22a8acfcd125d5bf39c
Kati Kyllonen <kati.a.kyllonen@helsinki.fi>
2013-08-31 20:12:32 +0300

    ratebeer projekti

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Ratebeer::Application.config.secret_token = '37a798083da8fc470ad8c85dab214c0007658c472740a6384e51ed478b7632edf641033aab8372bdf0053150c9c5623ad9018870384867b5f52554dc06df6715'
