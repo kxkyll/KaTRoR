6951304202580b22e968ca931f06a277101488ad
Kati Kyllonen <kati.a.kyllonen@helsinki.fi>
2013-09-15 16:29:06 +0300

    t2 rating-listaus

--- app/controllers/ratings_controller.rb
+++ app/controllers/ratings_controller.rb
@@ -1,4 +1,12 @@
 class RatingsController <ApplicationController
     def index
+        @ratings = Rating.all
+        @rating_count = Rating.count
+
+    respond_to do |format|
+      format.html # index.html.erb
+      format.json { render json: @ratings }
+     end
+    
     end
 end
