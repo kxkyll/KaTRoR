3486359eec7b05dc058554798583b405391d0b95
Kati Kyllonen <kati.a.kyllonen@helsinki.fi>
2013-10-06 15:53:38 +0300

    vk3 t8 validaattori vain 1 jäsenyys per henkilö

--- app/controllers/memberships_controller.rb
+++ app/controllers/memberships_controller.rb
@@ -45,12 +45,15 @@ class MembershipsController < ApplicationController
   # POST /memberships.json
   def create
     @membership = Membership.new(params[:membership])
+    @beer_clubs = BeerClub.all
+    @users = User.all
 
     respond_to do |format|
       if @membership.save
         format.html { redirect_to @membership, notice: 'Membership was successfully created.' }
         format.json { render json: @membership, status: :created, location: @membership }
       else
+        
         format.html { render action: "new" }
         format.json { render json: @membership.errors, status: :unprocessable_entity }
       end
