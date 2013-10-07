f96b02024c5ac91c082b692d3ec38b90e33b564a
Kati Kyllonen <kati.a.kyllonen@helsinki.fi>
2013-10-07 15:24:37 +0300

    vk3 t 14 salasanan validointi

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,6 +5,7 @@ class User < ActiveRecord::Base
   validates_uniqueness_of :username
   validates_length_of :username, :minimum => 3, :maximum => 15
   validates_length_of :password, :minimum => 4
+  validates_format_of :password, :with => /[0-9]+/
 
   has_many :ratings, :dependent => :destroy
   has_many :beers, :through => :ratings
