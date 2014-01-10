02a244f2129cb9e9e4c345c90e2fb865d8b0b71c
Kati Kyllonen <kati.a.kyllonen@helsinki.fi>
2014-01-10 18:57:20 +0200

    vk4 t6,7 ja 8

--- spec/features/ratings_spec.rb
+++ spec/features/ratings_spec.rb
@@ -2,28 +2,88 @@ require 'spec_helper'
 
 describe "Rating" do
     include OwnTestHelper
-  let!(:brewery) { FactoryGirl.create :brewery, :name => "Koff" }
-  let!(:beer1) { FactoryGirl.create :beer, :name => "iso 3", :brewery => brewery }
-  let!(:beer2) { FactoryGirl.create :beer, :name => "Karhu", :brewery => brewery }
-  let!(:user) { FactoryGirl.create :user }
+    let!(:brewery) { FactoryGirl.create :brewery, :name => "Koff" }
+    let!(:brewery2) { FactoryGirl.create :brewery, :name => "Karhula" }
+    let!(:beer1) { FactoryGirl.create :beer, :name => "iso 3", :brewery => brewery }
+    let!(:beer2) { FactoryGirl.create :beer, :name => "Karhu", :brewery => brewery2, :style =>'Ale' }
+    let!(:user) { FactoryGirl.create :user }
+    let!(:user2) { FactoryGirl.create :user, :username => "Pia",  :password => 'piapia2',  :password_confirmation => 'piapia2' }
 
-  before :each do
-    sign_in 'Pekka', 'foobar1'
-  end
+    before :each do
+      sign_in 'Pekka', 'foobar1'
+    end
 
-  it "when given, is registered to the beer and user who is signed in" do
-    visit new_rating_path
-    
-    #select(beer1.to_s, :from => 'rating[beer_id]')
-    
-    fill_in('rating[score]', :with => '15')
-    
-    expect{
-      click_button "Create Rating"
-    }.to change{Rating.count}.from(0).to(1)
+    it "when given, is registered to the beer and user who is signed in" do
+      visit new_rating_path
+      
+      select(beer1.name, :from => 'rating[beer_id]')
+      
+      fill_in('rating[score]', :with => '15')
+      
+      expect{
+        click_button "Create Rating"
+      }.to change{Rating.count}.from(0).to(1)
 
-    expect(user.ratings.count).to eq(1)
-    expect(beer1.ratings.count).to eq(1)
-    expect(beer1.average_rating).to eq(15.0)
-  end
+      expect(user.ratings.count).to eq(1)
+      expect(beer1.ratings.count).to eq(1)
+      expect(beer1.average_rating).to eq(15.0)
+    end
+
+    it "when two ratings, ratings are listed correctly in user page" do
+        visit new_rating_path
+        make_rate(beer1.name, 20)
+        
+        visit new_rating_path
+        make_rate(beer2.name, 22)
+
+        #save_and_open_page
+
+        expect(user.ratings.count).to eq(2)
+        expect(beer1.ratings.count).to eq(1)
+        expect(beer1.average_rating).to eq(20.0) 
+        expect(beer2.ratings.count).to eq(1)
+        expect(beer2.average_rating).to eq(22.0) 
+        expect(page).to have_content 'has given 2 ratings, average 21'
+        expect(page).to have_content 'favorite style Ale'
+        expect(page).to have_content 'favorite brewery Karhula'
+    end
+
+    it "when two ratings from two users, ratings are listed correctly in ratings page" do
+        visit new_rating_path
+        make_rate(beer1.name, 20)
+        
+        visit new_rating_path
+        make_rate(beer2.name, 22)
+
+        visit signout_path
+        sign_in 'Pia', 'piapia2' 
+
+        visit new_rating_path
+        make_rate(beer1.name, 12)
+       
+        visit new_rating_path
+        make_rate(beer2.name, 18)
+        
+        visit ratings_path
+        expect(page).to have_content 'Number of ratings: 4'
+    end
+
+    it "when user deletes one, rating is removed" do
+        visit new_rating_path
+        make_rate(beer1.name, 20)
+        
+        visit new_rating_path
+        make_rate(beer2.name, 22)
+
+        
+        expect(user.ratings.count).to eq(2)
+        expect(beer1.ratings.count).to eq(1)
+        expect(page).to have_content 'has given 2 ratings, average 21'
+        
+
+        find(:xpath, '//a[@href="/ratings/1"]').click
+        expect(user.ratings.count).to eq(1)
+        expect(beer1.ratings.count).to eq(0)
+        
+    end
 end
--- spec/support/helpers/own_test_helper.rb
+++ spec/support/helpers/own_test_helper.rb
@@ -6,4 +6,10 @@ module OwnTestHelper
         
         click_button('Log in')
     end
+
+    def make_rate(beername, score)
+        select(beername, :from => 'rating[beer_id]')
+        fill_in('rating[score]', :with => score)
+        click_button "Create Rating"
+    end
 end
